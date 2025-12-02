6f3ee4555a818f739b0424061174924e2ed8604c
Dell <Dell@DESKTOP-28DBUNU>
2025-12-02 19:25:31 +0530

    Add project files.

---
+++ Credit Card Transaction Analyzer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter the T_id:");
+        string t_id = Console.ReadLine();
+        Console.WriteLine("Enter the Amount:");
+        decimal amount = Convert.ToDecimal (Console.ReadLine());
+        Console.WriteLine("is the T_id is international:");
+        bool isinternational = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("Enter your rating");
+        double customerrating = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the transaction timestamp");
+        DateTime transaction timestamp =Convert.ToDateTime Console.ReadLine();
+        Console.WriteLine("Enter reward points:");
+        int rewardpoints =Convert.ToInt32(Console.ReadLine());
+    }
+}
